63ee0b07dbff71c7de80c526fd86ad795868d884
= <=>
2023-10-16 15:17:50 +0300

    commit

--- app.js
+++ app.js
@@ -1,25 +1,9 @@
-const path = require("path");
-const fs = require("fs");
-// fs.mkdir(path.join(__dirname, "tmp"), (err) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log("папка создана");
-// });
-
-const filePath = path.join(__dirname, "tmp", "tmp.txt");
-// fs.writeFile(filePath, "Привет студенты", (err) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log("файл создан");
-// });
-
-// fs.appendFile(filePath, "\nДо свидания, студенты", (err) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log("файл changed");
-// });
-
-module.exports = filePath;
+const express = require("express");
+const app = express("");
+const port = "3000";
+app.get("/", (reg, res) => {
+  res.end("Welcome");
+});
+app.listen(port, function () {
+  console.log("Сервер запущен порт" + port);
+});
