16b267f96bec4497b230d0b195e70743b5e5722b
DrobiazkoElizaveta <drobyazko.liza23@gmail.com>
2023-11-26 20:53:45 +0300

    1 commit

--- app.js
+++ app.js
@@ -2,27 +2,49 @@ const express = require("express");
 const favicon = require("express-favicon");
 const fs = require("fs");
 const path = require("path");
-const app = express("");
+const { nextTick } = require("process");
 const ejs = require("ejs");
 
+const app = express();
 const myRoutes = require("./routers/index_routers");
+const port = "3000";
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "views")));
+
+// const filePath = path.join(__dirname, "tmp", "1.txt");
+
+// fs.writeFile(filePath, `Сервер запущен. Порт: ${port}`, (err) => {
+//   if (err) console.error(err);
+//   console.log("файл создан");
+// });
+
+// function logger(port, router) {
+//   fs.appendFile(
+//     filePath,
+//     `\nЛогируем ping по адресу localhost:${port}${router}. Время: ${new Date()}`,
+//     (err) => {
+//       if (err) console.error(err);
+//       console.log("файл переписан");
+//     }
+//   );
+// }
+
+// console.log(app.get("env"));
 
 app.use(express.json());
-app.use(express.urlencoded({ extendend: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "css")));
+app.use(express.static(path.join(__dirname, "views")));
+
 app.use(
   "/css/bootstrap.css",
   express.static(
-    path.join(__dirname, "/CSS/bootstrap-5.3.2/dist/css/bootstrap.css")
-  )
-);
-app.use(
-  express.static(
-    path.join(__dirname, "/CSS/bootstrap-5.3.2/dist/js/bootstrap.js")
+    path.join(
+      __dirname,
+      "public/css/bootstrap-5.3.2/dist/css/bootstrap.min.css"
+    )
   )
 );
 
@@ -30,48 +52,33 @@ app.use(favicon(__dirname + "/public/favicon.png"));
 
 app.use(myRoutes);
 
-app.get("/", (req, res) => {
-  addLine("Pinging /");
-  res.sendFileSync(__dirname + "/public/index.html");
+app.listen(port, () => {
+  console.log(`listen on port ${port}`);
 });
-
-const port = "3000";
-// app.get("/", (req, res) => {
-//   addLine("Pinging /");
-//   res.sendFileSync(__dirname + "/public/index.html");
-// });
-
-app.get("/test", (req, res) => {
-  console.log("Прошли по пути тест");
-  res.end("Hello");
-});
-
-app.post("/test", (req, res) => {
-  addLine("Pinging /test");
-  console.log("Прошли по пути post test");
-  res.end("Прошли по пути post test");
-});
-
+app.get("env") == "production";
+console.log(app.get("env"));
+if (app.get("env") == "production") {
+  app.use((req, res, err) => {
+    res.status(err.status);
+    res.sendFile(err.message);
+  });
+}
+//ERROR HANDLER
 app.use((req, res, next) => {
-  const err = new Error("ERROR");
+  const err = new Error("Could't get path");
   err.status = 404;
   next(err);
 });
 
-console.log(app.get("env"));
 if (app.get("env") != "development") {
   app.use(function (err, req, res, next) {
-    // console.log(err.code, err.message);
+    console.log(err.status, err.message);
     res.status = 404;
-    let img = "http://localhost";
-    res.render("error", { err });
+    link = "https://centralsib.com/media/gallery/kukushka.jpg";
+    res.render("error.ejs", { err, link });
   });
 } else {
   app.use(function (err, req, res, next) {
     console.log(app.get("env"), err.status, err.message);
   });
 }
-
-app.listen(port, function () {
-  console.log("Сервер запущен порт " + port);
-});
--- routers/index_routers.js
+++ routers/index_routers.js
@@ -2,12 +2,28 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  addLine("Pinging /");
-  res.sendFileSync(__dirname + "../public/index.html");
+  res.end("/");
 });
-router.post("/", function (req, res) {});
+router.post("/", function (req, res) {
+  res.end("/");
+});
 
-router.get("/register", function (req, res) {});
+router.get("/register", function (req, res) {
+  res.render("register.ejs");
+});
 router.post("/register", function (req, res) {});
+
+router.get("/login", function (req, res) {
+  res.render("login.ejs");
+});
+router.post("/login", function (req, res) {});
+
+router.get("/test", function (req, res) {
+  res.end("/test");
+});
+router.post("/test", function (req, res) {
+  console.log("Прошли по пути post/test");
+  res.end("post/test");
+});
 
 module.exports = router;
