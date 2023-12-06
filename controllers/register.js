2b8697a5be716f55353f77c52b879340605322a7
DrobiazkoElizaveta <drobyazko.liza23@gmail.com>
2023-12-06 13:43:07 +0300

    1 commit

--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,11 @@
+const User = require("../models/User");
 exports.form = (req, res) => {
   res.render("register", {});
 };
 
 // module.exports = form;
+exports.submit = (req, res, next) => {
+  if (!Igor) return next;
+  const email = req.body.user.email;
+  User.create(req.body.user, cb);
+};
