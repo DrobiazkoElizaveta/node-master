1ebe34dafa71b6c27f7398ca6099b5f4f0cdbfe4
DrobiazkoElizaveta <drobyazko.liza23@gmail.com>
2023-12-18 10:59:58 +0300

    commit

--- app.js
+++ app.js
@@ -14,26 +14,6 @@ const port = "3000";
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-const filePath = path.join(__dirname, "tmp", "1.txt");
-
-// fs.writeFile(filePath, `Сервер запущен. Порт: ${port}`, (err) => {
-//   if (err) console.error(err);
-//   console.log("файл создан");
-// });
-
-// function logger(port, router) {
-//   fs.appendFile(
-//     filePath,
-//     `\nЛогируем ping по адресу localhost:${port}${router}. Время: ${new Date()}`,
-//     (err) => {
-//       if (err) console.error(err);
-//       console.log("файл переписан");
-//     }
-//   );
-// }
-
-// console.log(app.get("env"));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -80,17 +60,16 @@ app.use((req, res, next) => {
   next(err);
 });
 
-if (app.get("env") != "development") {
-  app.use(function (err, req, res, next) {
-    console.log(err.status, err.message);
-    res.status = 404;
-    link = "https://centralsib.com/media/gallery/kukushka.jpg";
-    res.render("error.ejs", { err, link });
-  });
-} else {
-  app.use(function (err, req, res, next) {
-    console.log(app.get("env"), err.status, err.message);
-  });
-}
+// if (app.get("env") != "development") {
+//   app.use(function (err, req, res, next) {
+//     console.log(err.status, err.message);
+//     res.status = 404;
+//     link = "https://centralsib.com/media/gallery/kukushka.jpg";
+//     res.render("error.ejs", { err, link });
+//   });
+// } else {
+//   app.use(function (err, req, res, next) {
+//     console.log(app.get("env"), err.status, err.message);
+//   });
+// }
 
-// kjsdhkdsh
