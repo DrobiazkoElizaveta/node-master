1a9743bf5dc76a251622613272212e4ccbcfc201
DrobiazkoElizaveta <drobyazko.liza23@gmail.com>
2024-03-20 10:29:13 +0300

    com

--- models/entry.js
+++ models/entry.js
@@ -1,107 +1,29 @@
-// const sqlite3 = require("sqlite3").verbose();
-// const db = new sqlite3.Database("test.db");
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("test.db");
 
-// const sql =
-//   "CREATE TABLE IF NOT EXISTS entries(id INTEGER PRIMARY KEY AUTOINCREMENT,username TEXT NOT NULL, title TEXT, content TEXT NOT NULL)";
-// db.run(sql);
-
-// class Entry {
-//   constructor() {}
-//   static create(data) {
-//     const sql =
-//       "INSERT INTO entries (username, title, content) VALUES (?, ?, ?)";
-//     db.run(sql, data.username, data.title, data.content);
-//   }
-//   static selectAll(cb) {
-//     db.all("SELECT * FROM entries", cb);
-//   }
-
-//   static getEntryById(entryId, cb) {
-//     const sql = "SELECT * FROM entries WHERE id = ?";
-//     db.get(sql, entryId, cb);
-//   }
-
-//   static delete(entryId, cb) {
-//     const sql = "DELETE FROM entries WHERE id = ?";
-//     db.run(sql, entryId, function (err) {
-//       if (err) {
-//         return cb(err);
-//       }
-//       cb(null);
-//     });
-//   }
-
-//   static update(entryId, newData, cb) {
-//     const checkExistenceSql = "SELECT * FROM entries WHERE id = ?";
-//     db.get(checkExistenceSql, entryId, (err, row) => {
-//       if (err) {
-//         return cb(err);
-//       }
-
-//       if (!row) {
-//         return cb(new Error("Entry not found"));
-//       }
-
-//       const updateSql =
-//         "UPDATE entries SET title = ?, content = ? WHERE id = ?";
-//       db.run(updateSql, newData.title, newData.content, entryId, cb);
-//     });
-//   }
-// }
-
-// module.exports = Entry;
-
-const mysql = require("mysql2");
-
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "test",
-  password: "2806",
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error("Error connecting to MySQL database", err);
-    return;
-  }
-  console.log("Connected to MySQL database");
-});
-db.query(
-  "CREATE TABLE IF NOT EXISTS entries(id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255) NOT NULL, title VARCHAR(255), content TEXT NOT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP)"
-);
+const sql =
+  "CREATE TABLE IF NOT EXISTS entries(id INTEGER PRIMARY KEY AUTOINCREMENT,username TEXT NOT NULL, title TEXT, content TEXT NOT NULL)";
+db.run(sql);
 
 class Entry {
   constructor() {}
-
   static create(data) {
     const sql =
-      "INSERT INTO entries (username, title, content, created_at) VALUES (?, ?, ?, CURRENT_TIMESTAMP)";
-    db.execute(sql, [data.username, data.title, data.content]);
+      "INSERT INTO entries (username, title, content) VALUES (?, ?, ?)";
+    db.run(sql, data.username, data.title, data.content);
   }
-
   static selectAll(cb) {
-    db.query("SELECT * FROM entries", (err, results) => {
-      if (err) {
-        return cb(err);
-      }
-      cb(null, results);
-    });
+    db.all("SELECT * FROM entries", cb);
   }
 
   static getEntryById(entryId, cb) {
     const sql = "SELECT * FROM entries WHERE id = ?";
-    db.query(sql, [entryId], (err, result) => {
-      if (err) {
-        return cb(err);
-      }
-      cb(null, result[0]);
-    });
+    db.get(sql, entryId, cb);
   }
 
   static delete(entryId, cb) {
     const sql = "DELETE FROM entries WHERE id = ?";
-    db.execute(sql, [entryId], (err, result) => {
+    db.run(sql, entryId, function (err) {
       if (err) {
         return cb(err);
       }
@@ -111,29 +33,107 @@ class Entry {
 
   static update(entryId, newData, cb) {
     const checkExistenceSql = "SELECT * FROM entries WHERE id = ?";
-    db.query(checkExistenceSql, [entryId], (err, rows) => {
+    db.get(checkExistenceSql, entryId, (err, row) => {
       if (err) {
         return cb(err);
       }
 
-      if (rows.length === 0) {
+      if (!row) {
         return cb(new Error("Entry not found"));
       }
 
       const updateSql =
         "UPDATE entries SET title = ?, content = ? WHERE id = ?";
-      db.execute(
-        updateSql,
-        [newData.title, newData.content, entryId],
-        (err, result) => {
-          if (err) {
-            return cb(err);
-          }
-          cb(null);
-        }
-      );
+      db.run(updateSql, newData.title, newData.content, entryId, cb);
     });
   }
 }
 
 module.exports = Entry;
+
+// const mysql = require("mysql2");
+
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   database: "test",
+//   password: "2806",
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.error("Error connecting to MySQL database", err);
+//     return;
+//   }
+//   console.log("Connected to MySQL database");
+// });
+// db.query(
+//   "CREATE TABLE IF NOT EXISTS entries(id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255) NOT NULL, title VARCHAR(255), content TEXT NOT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP)"
+// );
+
+// class Entry {
+//   constructor() {}
+
+//   static create(data) {
+//     const sql =
+//       "INSERT INTO entries (username, title, content, created_at) VALUES (?, ?, ?, CURRENT_TIMESTAMP)";
+//     db.execute(sql, [data.username, data.title, data.content]);
+//   }
+
+//   static selectAll(cb) {
+//     db.query("SELECT * FROM entries", (err, results) => {
+//       if (err) {
+//         return cb(err);
+//       }
+//       cb(null, results);
+//     });
+//   }
+
+//   static getEntryById(entryId, cb) {
+//     const sql = "SELECT * FROM entries WHERE id = ?";
+//     db.query(sql, [entryId], (err, result) => {
+//       if (err) {
+//         return cb(err);
+//       }
+//       cb(null, result[0]);
+//     });
+//   }
+
+//   static delete(entryId, cb) {
+//     const sql = "DELETE FROM entries WHERE id = ?";
+//     db.execute(sql, [entryId], (err, result) => {
+//       if (err) {
+//         return cb(err);
+//       }
+//       cb(null);
+//     });
+//   }
+
+//   static update(entryId, newData, cb) {
+//     const checkExistenceSql = "SELECT * FROM entries WHERE id = ?";
+//     db.query(checkExistenceSql, [entryId], (err, rows) => {
+//       if (err) {
+//         return cb(err);
+//       }
+
+//       if (rows.length === 0) {
+//         return cb(new Error("Entry not found"));
+//       }
+
+//       const updateSql =
+//         "UPDATE entries SET title = ?, content = ? WHERE id = ?";
+//       db.execute(
+//         updateSql,
+//         [newData.title, newData.content, entryId],
+//         (err, result) => {
+//           if (err) {
+//             return cb(err);
+//           }
+//           cb(null);
+//         }
+//       );
+//     });
+//   }
+// }
+
+// module.exports = Entry;
--- models/user.js
+++ models/user.js
@@ -1,132 +1,132 @@
-// const sqlite3 = require("sqlite3").verbose();
-// const db = new sqlite3.Database("test.db");
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("test.db");
 
-// const sql =
-//   "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT NOT NULL, email TEXT NOT NULL, password TEXT NOT NULL, age INT NOT NULL, isAdmin INT)";
+const sql =
+  "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT NOT NULL, email TEXT NOT NULL, password TEXT NOT NULL, age INT NOT NULL, isAdmin INT)";
 
-// db.run(sql);
+db.run(sql);
 
-// class User {
-//   constructor() {}
+class User {
+  constructor() {}
 
-//   static create(dataForm, cb) {
-//     const { name, email, password, age, isAdmin } = dataForm;
+  static create(dataForm, cb) {
+    const { name, email, password, age, isAdmin } = dataForm;
 
-//     db.run(
-//       "INSERT INTO users (name, email, password, age, isAdmin) VALUES (?, ?, ?, ?, ?)",
-//       [name, email, password, age, isAdmin],
-//       function (error) {
-//         if (error) return cb(error);
-//         cb(null, this.lastID);
-//       }
-//     );
-//   }
+    db.run(
+      "INSERT INTO users (name, email, password, age, isAdmin) VALUES (?, ?, ?, ?, ?)",
+      [name, email, password, age, isAdmin],
+      function (error) {
+        if (error) return cb(error);
+        cb(null, this.lastID);
+      }
+    );
+  }
 
-//   static findByEmail(email, cb) {
-//     db.get("SELECT * FROM users WHERE email = ?", email, cb);
-//   }
+  static findByEmail(email, cb) {
+    db.get("SELECT * FROM users WHERE email = ?", email, cb);
+  }
 
-//   static authentificate(dataForm, cb) {
-//     User.findByEmail(dataForm.email, (error, user) => {
-//       if (error) return cb(error);
-//       if (!user) return cb();
+  static authentificate(dataForm, cb) {
+    User.findByEmail(dataForm.email, (error, user) => {
+      if (error) return cb(error);
+      if (!user) return cb();
 
-//       if (dataForm.password === user.password) {
-//         cb(null, user);
-//       } else {
-//         cb();
-//       }
-//     });
-//   }
-// }
+      if (dataForm.password === user.password) {
+        cb(null, user);
+      } else {
+        cb();
+      }
+    });
+  }
+}
 
-// module.exports = User;
-
-const mysql = require('mysql2'); 
- 
-const connection = mysql.createConnection({ 
-  host: 'localhost', 
-  user: 'root', 
-  password: '2806', 
-  database: 'test' 
-}); 
- 
-connection.connect((err) => { 
-  if (err) { 
-    console.error('Error connecting to MySQL database', err); 
-    return; 
-  } 
-  console.log('Connected to MySQL database'); 
-}); 
- 
-const sql = ` 
-  CREATE TABLE IF NOT EXISTS users( 
-    id INT PRIMARY KEY AUTO_INCREMENT, 
-    name VARCHAR(255) NOT NULL, 
-    email VARCHAR(255) NOT NULL, 
-    password VARCHAR(255) NOT NULL, 
-    age INT NOT NULL, 
-    isAdmin INT 
-  ) 
-`; 
- 
-connection.query(sql, (err, results) => { 
-  if (err) { 
-    console.error('Error creating users table', err); 
-    return; 
-  } 
-  console.log('Users table created'); 
-}); 
- 
-class User { 
-  constructor() {} 
-   
-  static create(dataForm, cb) { 
-    const { name, email, password, age, isAdmin} = dataForm; 
-     
-    const sql = "INSERT INTO users (name, email, password, age, isAdmin) VALUES (?, ?, ?, ?, ?)"; 
-    connection.query(sql, [name, email, password, age, isAdmin], (err, results) => { 
-      if (err) { 
-        console.error('Error creating user', err); 
-        return cb(err); 
-      } 
-      console.log('User created'); 
-      cb(null, results.insertId); 
-    }); 
-  } 
-   
-  static findByEmail(email, cb) { 
-    const sql = "SELECT * FROM users WHERE email = ?"; 
-    connection.query(sql,[email], (err, rows) => { 
-      if (err) { 
-        console.error('Error finding user by email', err); 
-        return cb(err); 
-      } 
-      if (!rows || rows.length === 0) { 
-        return cb(null, null); 
-      } 
-      cb(null, rows[0]); 
-    }); 
-  } 
-   
-  static authentificate(dataForm, cb) { 
-    User.findByEmail(dataForm.email, (err, user) => { 
-      if (err) { 
-        console.error('Error authenticating user', err); 
-        return cb(err); 
-      } 
-       
-      if (!user) { 
-        return cb(); 
-      } 
-       
-      if (dataForm.password === user.password) { 
-        cb(null, user); 
-      } else { 
-        cb(); 
-      } 
-    }); 
-  } 
-} 
- 
 module.exports = User;
+
+// const mysql = require('mysql2'); 
+ 
+// const connection = mysql.createConnection({ 
+//   host: 'localhost', 
+//   user: 'root', 
+//   password: '2806', 
+//   database: 'test' 
+// }); 
+ 
+// connection.connect((err) => { 
+//   if (err) { 
+//     console.error('Error connecting to MySQL database', err); 
+//     return; 
+//   } 
+//   console.log('Connected to MySQL database'); 
+// }); 
+ 
+// const sql = ` 
+//   CREATE TABLE IF NOT EXISTS users( 
+//     id INT PRIMARY KEY AUTO_INCREMENT, 
+//     name VARCHAR(255) NOT NULL, 
+//     email VARCHAR(255) NOT NULL, 
+//     password VARCHAR(255) NOT NULL, 
+//     age INT NOT NULL, 
+//     isAdmin INT 
+//   ) 
+// `; 
+ 
+// connection.query(sql, (err, results) => { 
+//   if (err) { 
+//     console.error('Error creating users table', err); 
+//     return; 
+//   } 
+//   console.log('Users table created'); 
+// }); 
+ 
+// class User { 
+//   constructor() {} 
+   
+//   static create(dataForm, cb) { 
+//     const { name, email, password, age, isAdmin} = dataForm; 
+     
+//     const sql = "INSERT INTO users (name, email, password, age, isAdmin) VALUES (?, ?, ?, ?, ?)"; 
+//     connection.query(sql, [name, email, password, age, isAdmin], (err, results) => { 
+//       if (err) { 
+//         console.error('Error creating user', err); 
+//         return cb(err); 
+//       } 
+//       console.log('User created'); 
+//       cb(null, results.insertId); 
+//     }); 
+//   } 
+   
+//   static findByEmail(email, cb) { 
+//     const sql = "SELECT * FROM users WHERE email = ?"; 
+//     connection.query(sql,[email], (err, rows) => { 
+//       if (err) { 
+//         console.error('Error finding user by email', err); 
+//         return cb(err); 
+//       } 
+//       if (!rows || rows.length === 0) { 
+//         return cb(null, null); 
+//       } 
+//       cb(null, rows[0]); 
+//     }); 
+//   } 
+   
+//   static authentificate(dataForm, cb) { 
+//     User.findByEmail(dataForm.email, (err, user) => { 
+//       if (err) { 
+//         console.error('Error authenticating user', err); 
+//         return cb(err); 
+//       } 
+       
+//       if (!user) { 
+//         return cb(); 
+//       } 
+       
+//       if (dataForm.password === user.password) { 
+//         cb(null, user); 
+//       } else { 
+//         cb(); 
+//       } 
+//     }); 
+//   } 
+// } 
+ 
+// module.exports = User;
